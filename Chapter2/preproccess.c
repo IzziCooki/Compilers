0b0cc033936bd759075600cb94c5060c6deccf83
Dean Hauser <deanhauser10@gmail.com>
2025-12-15 11:06:03 -0500

    First Commit

---
+++ Chapter2/preproccess.c
@@ -0,0 +1,14 @@
+// Header file for input output functions
+#include <stdio.h>
+
+// Main function: entry point for execution
+int main() {
+
+    // Writing print statement to print hello world
+    int x; int y; int z;
+    x = 5;
+    y = 10;
+    z = x + y;
+    printf("5 + 10 = %d\n", z);
+    return 0;
+}
